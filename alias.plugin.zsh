17bcc20bfb790fb4b5b7e8671bc4aac3e56f354f
ndxbn <kota_suzuki@dwango.co.jp>
2018-04-15 06:21:38 +0900

    first

---
+++ alias.plugin.zsh
@@ -0,0 +1,17 @@
+# ls
+alias ls="ls -G"
+alias la="ls -AGh"
+alias lt="ls -AGlh"
+alias ll="ls -lG"
+
+# shorthands
+alias zshrc='$EDITOR ~/.zshrc'
+alias t='tail -f'
+
+# default options
+alias grep='grep --color'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
---
+++ hosts.plugin.zsh
@@ -0,0 +1,2 @@
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
